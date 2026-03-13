4f18eecc9db0203d4d7b0cf0f0e6e906b1172944
Robyn <robynmccallum11@gmail.com>
2026-03-13 23:08:21 +0000

    Removed debugging code from my scripts

--- Assets/Pawsitive Readings/Script/Robyn's Scripts/DieselButtonInteraction.cs
+++ Assets/Pawsitive Readings/Script/Robyn's Scripts/DieselButtonInteraction.cs
@@ -5,7 +5,6 @@ public class DieselButtonInteraction : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        Debug.Log("Play Animation");
         gameObject.GetComponent<Animator>().Play("Push");
         gameObject.GetComponent<AudioSource>().Play();
     }//end Interact
--- Assets/Pawsitive Readings/Script/Robyn's Scripts/Interactor.cs
+++ Assets/Pawsitive Readings/Script/Robyn's Scripts/Interactor.cs
@@ -19,15 +19,11 @@ public class Interactor : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Input");
             Ray r = new Ray(interactorSource.position, interactorSource.forward);
-            Debug.DrawRay(interactorSource.position, interactorSource.forward * interactorRange, Color.red, 1.0f); // Visualize ray
             if (Physics.Raycast(r, out RaycastHit hitInfo, interactorRange))
             {
-                Debug.Log("Raycast Hit");
                 if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
                 {
-                    Debug.Log("Object Found");
                     interactObj.Interact();
                 }//end if
             }//end if
